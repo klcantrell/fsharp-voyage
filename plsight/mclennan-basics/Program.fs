a3300619634f69606ea7ae9403a13571832cec7a
klcantrell <cantrellkalalau@gmail.com>
2019-10-22 21:34:18 -0400

    plsight/mclennan-basics: look at some primitive types

--- plsight/mclennan-basics/Program.fs
+++ plsight/mclennan-basics/Program.fs
@@ -15,6 +15,15 @@ printfn "%s %i" "minus1Times2" (minus1Times2 9)
 printfn "%s %i" "anotherMinus1Times" (9 |> anotherMinus1Times2)
 printfn "%s %i" "checking pipe precedence" ((9 |> minus1) + times2 2)
 
+//1 + 3.14 |> ignore //this won't compile
+
+printfn "%f" (float "3.14")
+
+printfn "%s" "It was the best of times,
+it was the worst of times,".[0..5]
+
+printfn "%b" ("03249" |> String.forall System.Char.IsDigit)
+
 [<EntryPoint>]
 let main argv =
     printArray argv
