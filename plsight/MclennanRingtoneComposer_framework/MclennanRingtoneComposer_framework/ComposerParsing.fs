f585939c0aa8e5f0638e72404df1c7fe74163591
klcantrell <cantrellkalalau@gmail.com>
2019-10-27 09:12:01 -0400

    plsight/MclennanRingtoneComposer: build up a parser for a composition

--- plsight/MclennanRingtoneComposer_framework/MclennanRingtoneComposer_framework/ComposerParsing.fs
+++ plsight/MclennanRingtoneComposer_framework/MclennanRingtoneComposer_framework/ComposerParsing.fs
@@ -31,4 +31,45 @@ let plength =
         pextended
         (fun t e -> { fraction = t; extended = e })
 
-printfn "%A" (test plength aspiration)
+let pnotsharpablenote = anyOf "be" |>> (function
+                            | 'b' -> B
+                            | 'e' -> E
+                            | unknown -> sprintf "Unknown note %c" unknown |> failwith)
+
+
+let psharp = (stringReturn "#" true) <|> (stringReturn "" false)
+
+let psharpnote = pipe2
+                    psharp
+                    (anyOf "acdfg")
+                    (fun isSharp note ->
+                        match (isSharp, note) with
+                        | (false, 'a') -> A
+                        | (true, 'a') -> ASharp
+                        | (false, 'c') -> C
+                        | (true, 'c') -> CSharp
+                        | (false, 'd') -> D
+                        | (true, 'd') -> DSharp
+                        | (false, 'f') -> F
+                        | (true, 'f') -> FSharp
+                        | (false, 'g') -> G
+                        | (true, 'g') -> GSharp
+                        | (_, unknown) -> sprintf "Unknown note %c" unknown |> failwith)
+
+let pnote = pnotsharpablenote <|> psharpnote
+
+let poctave = anyOf "123" |>> (function
+                 | '1' -> One
+                 | '2' -> Two
+                 | '3' -> Three
+                 | unknown -> sprintf "Unknown octave %c" unknown |> failwith)
+
+let ptone = pipe2 pnote poctave (fun n o -> Tone(note = n, octave = o))
+
+let prest = stringReturn "-" Rest
+
+let ptoken = pipe2 plength (prest <|> ptone) (fun l s -> { length = l; sound = s })
+
+let pscore = sepBy ptoken (pstring " ")
+
+printfn "%A" (test ptoken aspiration)
