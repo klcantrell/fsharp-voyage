dad90792ff53281fffa9b876658dbbf99159d753
klcantrell <cantrellkalalau@gmail.com>
2019-11-16 14:06:50 -0500

    HelloWorldWebAppWithRazorViews: practice web app with giraffe framework

---
+++ HelloWorldWebAppWithRazorViews/HelloWorldWebAppWithRazorViews/WebApp.fs
@@ -0,0 +1,23 @@
+﻿module WebApp
+
+open Giraffe
+open Giraffe.GiraffeViewEngine
+
+[<CLIMutable>]
+type PracticeModel =
+    {
+        WelcomeText : string
+    }
+
+let practiceModel = { WelcomeText = "Sup y'all" }
+
+let view (model : PracticeModel) =
+    h1 [] [ str model.WelcomeText]
+
+let webApp : HttpHandler = 
+    choose [
+        GET >=> 
+            choose [
+                route "/" >=> (view practiceModel |> htmlView)
+            ]
+    ]
