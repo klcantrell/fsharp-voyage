775b381cb662d2d550ce0ce8638423d10b6f5322
klcantrell <cantrellkalalau@gmail.com>
2020-01-06 22:37:56 -0500

    GenCssModulesType: add change handler for CSS files
    Currently, handler just prints rules out to console.

--- GenCssModulesType/GenCssModulesType/Program.fs
+++ GenCssModulesType/GenCssModulesType/Program.fs
@@ -2,18 +2,41 @@
 open System.IO
 open Lib
 
+let printWithColor (color: ConsoleColor) (message: string) =
+    Console.ForegroundColor <- color
+    Console.WriteLine(message)
+    Console.ResetColor()
+
+let printErrorWithColor (color: ConsoleColor) (message: string) =
+    Console.ForegroundColor <- color
+    Console.Error.WriteLine(message)
+    Console.ResetColor()
+
+let handleOnChange path =
+    let rec handler retries =
+        if retries > 10_000
+        then
+             "Could not open file" |> printErrorWithColor ConsoleColor.DarkRed 
+        else
+            try
+                File.ReadAllText path
+                |> parseRules
+                |> List.iter(
+                    fun rule -> ("The rule is " + rule) |> printWithColor ConsoleColor.Magenta)
+            with
+                | :? System.IO.IOException -> 
+                    ("Retrying, attempt: " + (retries + 1).ToString()) |> printWithColor ConsoleColor.Yellow
+                    handler (retries + 1)
+    handler 0
+
 [<EntryPoint>]
 let main argv =
-
-    Lib.testInput
-    |> parseRules
-    |> List.iter(fun rule -> Console.WriteLine("The rule is " + rule))
-
     let fileSystemWatcher = new FileSystemWatcher()
     fileSystemWatcher.Path <- "."
     fileSystemWatcher.Filter <- "*.css"
-    fileSystemWatcher.Changed.Add(fun _ -> Console.WriteLine("A css file changed"))
+    fileSystemWatcher.Changed.Add(fun e -> handleOnChange e.FullPath)
     fileSystemWatcher.EnableRaisingEvents <- true
+
     while (Console.Read() |> Convert.ToChar <>  'q') do
         ()
     0
