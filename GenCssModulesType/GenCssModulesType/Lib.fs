14d2ffa9d32d51a601cadba47a1ec4ef17b172ec
klcantrell <cantrellkalalau@gmail.com>
2020-01-06 19:53:31 -0500

    GenCssModulesType: initial testing w/ CoreRT

---
+++ GenCssModulesType/GenCssModulesType/Lib.fs
@@ -0,0 +1,17 @@
+﻿module Lib
+
+open System.Text.RegularExpressions
+
+let parseRules str =
+    let rulePattern = @"\.\w+"
+    let matches = Regex.Matches(str, rulePattern)
+    if matches.Count > 0 then [for m in matches -> m.Value] else []
+
+let testInput = "
+    .sup
+        color: blue;
+    }
+    .dude {
+        color: red;
+    }
+"
---
+++ GenCssModulesType/GenCssModulesType/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.IO
+open Lib
+
+[<EntryPoint>]
+let main argv =
+
+    Lib.testInput
+    |> parseRules
+    |> List.iter(fun rule -> Console.WriteLine("The rule is " + rule))
+
+    let fileSystemWatcher = new FileSystemWatcher()
+    fileSystemWatcher.Path <- "."
+    fileSystemWatcher.Filter <- "*.css"
+    fileSystemWatcher.Changed.Add(fun _ -> Console.WriteLine("A css file changed"))
+    fileSystemWatcher.EnableRaisingEvents <- true
+    while (Console.Read() |> Convert.ToChar <>  'q') do
+        ()
+    0
+
